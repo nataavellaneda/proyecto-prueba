849499f8a4fc0e4a185a880980006883a7258931
nataavellaneda <avellanedagutierreznatalia@gmail.com>
2021-09-24 21:36:26 -0500

    primer commit

---
+++ script.js/jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.1').css("color","green");
+
+    $('.2').css("color","blue");
+
+    $('.3').css("color","red");
+
+
+    $('.textofooter').css("font-family","cursive").css("font-size","20px").css("font-style","unset").css("color","black")
+    .css("background","darksalmon").css("text-align","center");
+
+
+
+});
+
+
